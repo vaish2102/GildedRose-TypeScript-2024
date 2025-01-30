730c1f2289bba49ecaa7a609df9860ab6800ce62
Vaishnavi Rajasekar <vaishnavi.rajasekar@softwire.com>
2025-01-30 12:47:54 +0000

    Added comments in gilded rose and unit test for quality > 0

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -16,24 +16,32 @@ export class GildedRose {
   constructor(items = [] as Array<Item>) {
     this.items = items;
   }
+//TO DO:
+//Sulfuras- quality to be 80
 
+//Update Quality
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
+      //Quality goes down for all items other than Aged Brie , Sulfuras,Backstage pass
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1
           }
         }
-      } else {
+      } else {// Aged Brie or Backstage pass
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1
+          //Backstage pass
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+            //Backstage pass - sellIn less than or equal to 10 days , quality increases by 2
             if (this.items[i].sellIn < 11) {
+              // quality shd never be greater than 50
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1
               }
             }
+            //Backstage pass - sellIn less than or equal to 5 days, quality increases by 3
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1
@@ -42,21 +50,25 @@ export class GildedRose {
           }
         }
       }
+      //For all items except Sulfuras - sellIn decreases by 1
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
+      //SellIn has expired
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+            //Quality is never negative
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                //All items other than Aged Brie , Sulfuras,Backstage pass,after sell by date,quality decreases by 2 (1 has been decreased above)
                 this.items[i].quality = this.items[i].quality - 1
               }
             }
-          } else {
+          } else { //Backstage pass - quality drops to 0 after concert (sell by date)
             this.items[i].quality = this.items[i].quality - this.items[i].quality
           }
-        } else {
+        } else {//Aged Brie - increases in quality after sell by date(quality maintained up to 50)
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1
           }
--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,11 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+  //All items should have quality greater than or equal to zero
+  it('should not allow any item that has quality less than 0', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 10, -5),new Item('Backstage passes to a TAFKAL80ETC concert',3, 6)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+      expect(items[0].quality).toBeGreaterThanOrEqual(0);
   });
 });
+
