c32144270089fd5f61c39a0339af444f245d189c
Vaishnavi Rajasekar <vaishnavi.rajasekar@softwire.com>
2025-01-31 11:45:18 +0000

    Part 2 - conjured mana cake added , Added more test cases

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -22,12 +22,15 @@ export class GildedRose {
     for (let i = 0; i < this.items.length; i++) {
       switch(this.items[i].name){
         case "Aged Brie":
-        this.updateValuesForAgedBrie(this.items[i]);
-        break;
+          this.updateValuesForAgedBrie(this.items[i]);
+          break;
         case "Backstage passes to a TAFKAL80ETC concert":
         this.updateValuesForBackstage(this.items[i]);
         break;
         case "Sulfuras, Hand of Ragnaros":
+        break;
+        case "Conjured Mana Cake":
+        this.updateValuesForConjured(this.items[i]);
         break;
         default:
         this.updateValuesForOtherItems(this.items[i]);
@@ -47,19 +50,21 @@ export class GildedRose {
   
   updateValuesForBackstage(item:Item){
     item.sellIn = item.sellIn - 1;
-    item.quality = item.quality === 50 ? item.quality : item.quality + 1;
     let sellIn = item.sellIn;
-    //Backstage pass - sellIn less than or equal to 10 days , quality increases by 2
-    if (sellIn < 11) {
-      item.quality = item.quality === 50 ? item.quality : item.quality + 1;
-    }
-    //Backstage pass - sellIn less than or equal to 5 days, quality increases by 3
-    if (sellIn < 6) {
-      item.quality = item.quality === 50 ? item.quality : item.quality + 1;
-    }
-    if(sellIn < 0){     //changed to <0 as sell can be negative as consistent with original code! 
+    if(sellIn < 0){     //sellIn < 0 , quality set to 0 
       item.quality = 0;
     }
+    else{
+      item.quality = item.quality === 50 ? item.quality : item.quality + 1;
+      //Backstage pass - sellIn less than or equal to 10 days , quality increases by 2
+      if (sellIn < 11) {
+        item.quality = item.quality === 50 ? item.quality : item.quality + 1;
+      }
+      //Backstage pass - sellIn less than or equal to 5 days, quality increases by 3
+      if (sellIn < 6) {
+        item.quality = item.quality === 50 ? item.quality : item.quality + 1;
+      }
+    }  
   }
 
   updateValuesForAgedBrie(item:Item){
@@ -67,4 +72,37 @@ export class GildedRose {
     item.quality = item.quality === 50 ? item.quality : item.quality + 1;
   }
 
+  updateValuesForConjured(item:Item){
+    item.sellIn = item.sellIn - 1;
+    item.quality = item.quality === 0 ? item.quality : item.quality - 2;
+    if (item.sellIn < 0){
+      item.quality = item.quality === 0 ? item.quality : item.quality - 2;
+    }
+    
+  }
+
 }
+
+//Aged Brie - sellIn goes down by 1
+//Aged Brie - quality goes up by 1
+//Aged Brie - quality shouldn't cross 50
+
+//Backstage - sellIn goes down by 1
+//Backstage - quality is 0 if sellIn < 0
+//Backstage - quality goes up by 3 if sellIn = 0
+//Backstage - quality goes up by 3 if sellIn < 6
+//Backstage - quality goes up by 2 if sellIn > 5 and sellIn < 11
+//Backstage - quality goes up by 1 if sellIn > 10
+//Backstage - quality should not be more than 50
+
+//Other items - sellIn goes down by 1
+//Other items - quality goes down by 1 if sellin > 0
+//Other items - quality goes down by 2 if sellIn < 0
+//Other items - quality does down by 1 if sellIn = 0
+
+
+//Conjured Mana cake - sellIn goes down by 1
+//Conjured Mana cake - quality goes down by 2 if sellIn > 0
+//Conjured Mana cake - quality goes down by 4 if sellIn < 0
+//Conjured Mana cake - quality does down by 2 if sellIn = 0
+//Conjured Mana cake - quality should not be less than 0
--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,12 +1,22 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  //All items should have quality greater than or equal to zero
-  it('should not reduce the quality below 0', () => {
-    const gildedRose = new GildedRose([new Item('ItemOne', 10, 0)]);
+  //Other items / Conjured Mana Cake - quality should not be less than 0
+  it ('should not reduce the quality below 0', () => {
+    const gildedRose = new GildedRose([new Item('ItemOne', 10, 0),new Item('ItemTwo', 0, 0),new Item('ItemThree', -3, 0), new Item('Conjured Mana Cake',5,0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBeGreaterThanOrEqual(0);
+    let results = items.map((elem)=> elem.quality)
+    expect(results).toEqual([0,0,0,0]);
   });
+
+  //Aged Brie and Backstage - quality should not be more than 50
+  it('should not increase the quality above 50' , () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 50), new Item('Aged Brie', 15, 50)]);
+    const items = gildedRose.updateQuality();
+    let results = items.map((elem) => elem.quality);
+    expect(results).toEqual([50,50]);
+  });
+
 
   //Quality should go down by one for all items except Aged Brie, Sulfuras and Backstage
   it('should decrease the quality by one for all items except Aged Brie, Sulfuras and Backstage passes to a TAFKAL80ETC concert', () => {
@@ -41,5 +51,11 @@ describe('Gilded Rose', () => {
     const gildedRose = new GildedRose([new Item('Aged Brie', 20, 7)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(8);
+  });
+  
+  it('should decrease the quality by two for sellIn greater than or equal to 0 for Conjured Mana Cake ' ,() =>{
+    const gildedRose = new GildedRose([new Item('Conjured Mana Cake',3,5)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(3);
   });
 });
